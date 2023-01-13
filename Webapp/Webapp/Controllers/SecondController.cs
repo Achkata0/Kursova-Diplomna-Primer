ce1f556bf7c71ae70ced0053456fc957e8caa601
Achkata0 <1234@gmail.com>
2023-01-13 10:32:00 +0200

    Start for asp kursova

---
+++ Webapp/Webapp/Controllers/SecondController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Webapp.Controllers
+{
+    public class SecondController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        } 
+
+    }
+}
